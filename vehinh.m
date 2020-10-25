79c1ecd45d4e6e5d1c5bb8639f683b1c30b30f9a
nguyentrungnghia1998 <thandongdoinon98@gmail.com>
2020-10-25 10:28:25 +0700

    lan1

---
+++ vehinh.m
@@ -0,0 +1,11 @@
+i=6;
+hold on;
+plot(case1.time,case1.data(:,i));
+pause(3);
+plot(case2.time,case2.data(:,i));
+pause(3);
+plot(case3.time,case3.data(:,i));
+pause(3);
+plot(case4.time,case4.data(:,i));
+pause(3);
+plot(case5.time,case5.data(:,i));
